c5886797efca62fb5672aa20b12bb4ee3a693743
wizardleen <wizardleen@gmail.com>
2025-09-06 22:56:24 +0800

    fix: Fix unit test failures

--- server/src/test/java/org/kiwi/console/generate/MockChat.java
+++ server/src/test/java/org/kiwi/console/generate/MockChat.java
@@ -22,7 +22,7 @@ class MockChat implements Chat {
             case PAGE_CREATE -> "@@  src/App.tsx @@\n// " + prompt.appName() + prompt.prompt();
             case PAGE_UPDATE -> "@@  src/App.tsx @@\n" + prompt.prompt();
             case PAGE_COMMIT_MSG -> "commit front";
-            case AUTO_TEST -> "PASSED";
+            case AUTO_TEST -> "ACCEPT\n{}";
             case FIX -> requireNonNull(lastCode).replace("Error", "Fixed");
             case COMMIT_MSG -> "commit";
         };
